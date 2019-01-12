457b8e146586a26429950547a62b4a279e4fa3b2
huyongbo <995401522@qq.com>
2019-01-12 03:41:16 +0000

    切割头部和尾部子视图

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -20,5 +20,4 @@ class StaticPagesController extends Controller
     {
         return view('static_pages/about');
     }
-    //
 }
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,6 +1,6 @@
-    @extends('layouts.default')
-    @section('title','帮助')
+@extends('layouts.default')
+@section('title', '帮助')
 
-    @section('content')
-        <h1>帮助页</h1>
-    @stop
+@section('content')
+  <h1>帮助页</h1>
+@stop
