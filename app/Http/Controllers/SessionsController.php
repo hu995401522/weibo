1be0d74bb55ea06719bb124529eeb40c35eeef62
huyongbo <995401522@qq.com>
2019-01-17 03:50:28 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,14 @@ use Auth;
 
 class SessionsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -22,12 +30,12 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
        }
-       return;
     }
         public function destroy()
         {
@@ -35,4 +43,9 @@ class SessionsController extends Controller
         session()->flash('success', '您已成功退出！');
         return redirect('login');
         }
+
+
+
 }
+
+
